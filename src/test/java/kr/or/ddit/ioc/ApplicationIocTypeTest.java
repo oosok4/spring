6c68d43ed5899836ddf6b2f7b42d1efaba8ac500
oosok4 <oosok4@naver.com>
2019-06-20 17:11:12 +0900

    spring 타입변환  - customDateEditor  - conversionService **두가지를 모두 설정할 경우 customDateEditor가 실행

---
+++ src/test/java/kr/or/ddit/ioc/ApplicationIocTypeTest.java
@@ -0,0 +1,49 @@
+package kr.or.ddit.ioc;
+
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.user.model.UserVo;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:kr/or/ddit/ioc/application-ioc-type.xml")
+public class ApplicationIocTypeTest {
+	
+	
+	//userVo라는 이름의 스프링빈이 정상적으로 생성 되었는지 확인.
+	private static final Logger logger = LoggerFactory.getLogger(ApplicationIocTypeTest.class);
+	
+	@Resource(name="userVo")
+	private UserVo userVo;
+	
+	@Test
+	public void test() {
+		/***Given***/
+
+		Date birth = userVo.getBirth();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String birth_str = sdf.format(birth);
+		/***When***/
+
+		/***Then***/
+		assertNotNull(userVo); 
+		logger.debug("birth : {}",birth_str);
+		assertEquals("2019-08-08", birth_str);
+		
+
+	
+	}
+
+}
